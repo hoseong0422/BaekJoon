4c9ab080a6c363ea16b9b0fb5e25ffabbdbba652
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2022-06-24 08:48:39 +0900

    [Silver V] Title: 셀프 넘버, Time: 76 ms, Memory: 31352 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,11 +1,14 @@
-numbers = set(range(1, 10001))
-remove_num = set() # 삭제하기 위한 숫자 set
+def find_self_num(n):
+    numbers = set(range(1, n+1))
+    remove_num = set() # 삭제하기 위한 숫자 set
 
-for i in numbers:
-    for j in str(i):
-        i += int(j) # 생성자가 있는 숫자
-    remove_num.add(i) # add: set에 요소를 추가
+    for i in numbers:
+       for j in str(i):
+           i += int(j) # 생성자가 있는 숫자
+       remove_num.add(i) # add: set에 요소를 추가
 
-self_num = (numbers - remove_num)
-for i in sorted(self_num):
-    print(i)
+    self_num = (numbers - remove_num)
+    for i in sorted(self_num):
+        print(i)
+
+find_self_num(10000)
