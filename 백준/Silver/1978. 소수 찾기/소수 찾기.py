40348170debddf12a905ace3e2b51d7f091ea4f2
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2022-09-14 23:55:17 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -6,10 +6,10 @@ for i in data:
     cnt = 0
     if (i == 1):    
         continue
-    for j in range(2 , i+1):
+    for j in range(2 , i):
         if (i % j == 0):
             cnt += 1
-    if (cnt == 1):
+    if (cnt == 0):
         prime_cnt += 1
 
 print(prime_cnt)
