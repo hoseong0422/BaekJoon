1badeb2694752f779c0a32041f31866ac3bbfb66
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2023-03-12 15:47:09 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,14 @@
+"""
+이항계수 = n! / k! (n - k!)
+"""
+
+def factorial(n):
+    if n == 0:
+        return 1
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+n, k = map(int, input().split())
+print(factorial(n) // (factorial(k) * factorial(n - k)))
