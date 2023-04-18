6d4a4be534acb82ef83e6fe19b8b30215228e895
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2023-04-18 22:38:48 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,5 @@
+data = {'NLCS' : 'North London Collegiate School', 
+        'BHA' : 'Branksome Hall Asia', 
+        'KIS' : 'Korea International School',
+        'SJA' : 'St. Johnsbury Academy'}
+print(data[input()])
