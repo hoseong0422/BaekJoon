d5bdc774fc5b363e35ec8953289d73b022cea74c
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2023-04-22 20:15:09 +0900

    [Bronze V] Title: Serca, Time: 228 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,13 @@
+N = int(input())
+data = """ @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     """
+    
+for _ in range(N):
+    print(data)
