e82b63a238c902e9193fe5c6a7e69d41220956a6
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2023-05-28 21:32:37 +0900

    [unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        while True:
+            if i == 1:
+                break
+            if i % 2 == 0:
+                i //= 2
+                answer += 1
+            else:
+                i -= 1
+                i //= 2
+                answer += 1
+    return answer
