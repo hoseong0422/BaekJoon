5292805cf3657b79a5f983428284b4165268e230
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2023-09-18 20:42:26 +0900

    [Bronze II] Title: 카이사르 암호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5598. 카이사르 암호/카이사르 암호.py
@@ -0,0 +1,7 @@
+before = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+after = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C']
+answer = ''
+data = input()
+for i in data:
+    answer += before[after.index(i)]
+print(answer)
