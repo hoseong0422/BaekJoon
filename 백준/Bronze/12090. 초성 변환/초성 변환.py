7f1c452a338783e9feac65398ae4558e0b1961e1
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2023-11-14 21:50:31 +0900

    [Bronze I] Title: 초성 변환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/12090. 초성 변환/초성 변환.py
@@ -0,0 +1,10 @@
+name = input()
+
+CHOSEONG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+answer = []
+for w in name:
+    choseong = (ord(w) - ord('가'))//588
+    answer.append(CHOSEONG_LIST[choseong])
+
+print(*answer, sep='')
