4034a0252a337a433e4bd563adf9af8fed536020
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2023-12-11 21:18:09 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -0,0 +1,17 @@
+N = int(input())
+answer = ''
+
+for i in range(N):
+    input_data = input()
+    lst_data = [i for i in input_data]
+    if i == 0:
+        answer = lst_data[:]
+        continue
+    else:
+        for j in range(len(answer)):
+            if answer[j] == '?':
+                continue
+            elif lst_data[j] != answer[j]:
+                answer[j] = '?'
+
+print(*answer, sep='')
