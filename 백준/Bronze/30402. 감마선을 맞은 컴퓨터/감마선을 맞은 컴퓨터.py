a2837fd744d10b5616dbf9ebf9d3ed1dcbe70d16
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2024-01-04 20:24:48 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for _ in range(15):
+    data = list(map(str, input().split()))
+    if 'w' in data:
+        print('chunbae')
+        break
+    elif 'b' in data:
+        print('nabi')
+        break
+    elif 'g' in data:
+        print('yeongcheol')
+        break
