a8676fc6e2bd70bd9d2c3bf0897542630e9b4be6
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2024-01-28 12:46:25 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -0,0 +1,12 @@
+target = int(input())
+n_2 = 0
+n_1 = 1
+i = 2
+while True:
+    now = n_2 + n_1
+    i += 1
+    n_2 = n_1
+    n_1 = now 
+    if i > target:
+        print(now, target-2, sep=' ')
+        break
