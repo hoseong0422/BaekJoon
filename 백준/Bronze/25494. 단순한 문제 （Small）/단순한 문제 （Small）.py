cbd2edff4c9aeb0dc7f66e81a35fe4427400aceb
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2024-03-16 20:50:57 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 3000 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for _ in range(N):
+    a, b, c = map(int, input().split())
+    cnt = 0
+    for i in range(1, a + 1):
+        for j in range(1, b + 1):
+            for k in range(1, c + 1):
+                if i % j == j % k == k % i:
+                    cnt += 1
+    print(cnt)
