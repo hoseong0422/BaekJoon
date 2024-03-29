44269c29c14d81dd289f4e359f243702befcd00e
Hoseong <hoseong0422@gmail.com>
2024-03-29 21:34:59 +0900

    add gitignore

--- 프로그래머스/lv1/131128. 숫자 짝꿍(temp)/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍(temp)/숫자 짝꿍.py
@@ -1,9 +1,19 @@
-from itertools import combinations
+def solution(X, Y):
+    answer = ''
 
-def solution(number):
-    answer = 0
-    combination = list(combinations(number, 3))
-    for combi in combination:
-        if sum(combi) == 0:
-            answer += 1
-    return answer
+    temp = [i for i in X]
+    for i in list(set(temp)):
+        if X.count(i) > 0 and Y.count(i) > 0:
+            if X.count(i) > Y.count(i):
+                answer += i * Y.count(i)
+            else:
+                answer += i * X.count(i)
+
+    answer_lst = [i for i in answer]
+    answer_lst.sort(reverse=True)
+    if len(answer_lst) != 0:
+        return str(int(''.join(answer_lst)))
+    else:
+        return '-1'
+    
+# 풀리긴 하지만 시간초과 발생 나중에 해결해보기    
