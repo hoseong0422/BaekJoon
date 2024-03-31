086b776c732d14e470a5838668bfc57acf73c027
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2024-03-31 20:59:14 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,10 +1,10 @@
-def get_gcd(n, m):
-    while m:
-        n, m = m, n % m
-    return n
+def get_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
-def get_lcd(n, m):
-    return n * m // get_gcd(n, m)
+def get_lcd(a, b):
+    return a * b // get_gcd(a, b)
 
 a, b = map(int, input().split())
 print(get_gcd(a, b), get_lcd(a, b), sep='\n')
