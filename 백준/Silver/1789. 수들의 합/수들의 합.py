0e7ec4e627d6fc6d85c5db8cff68b679a24f72a1
Hoseong <hoseong0422@gmail.com>
2024-04-07 16:09:39 +0900

    풀이 수정

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,14 +1,12 @@
 N = int(input())
 
 S = 0
-while True:
-    for i in range(1, N+1):
+for i in range(1, N+1):
 
-        S += i
-        if S == N:
-            print(i)
-            break
-        elif S > N:
-            print(i - 1)
-            break
-    break
+    S += i
+    if S == N:
+        print(i)
+        break
+    elif S > N:
+        print(i - 1)
+        break
