99171f4192a96f70c4ec3423ded2a33a340544f3
Hoseong <hoseong0422@gmail.com>
2024-04-18 23:16:52 +0900

    다시 풀어보기

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,3 +1,4 @@
+# 시간 단축 방법 좀더 고민해보기
 from string import ascii_lowercase
 
 lower_list = list(ascii_lowercase)
@@ -5,8 +6,12 @@ lower_list = list(ascii_lowercase)
 N = int(input())
 data = input()
 
+lower_dict = {}
+for i in range(len(lower_list)):
+    lower_dict[lower_list[i]] = i + 1
+
 answer = 0
 
 for i in range(N):
-    answer += (lower_list.index(data[i]) + 1) * 31 ** i
+    answer += lower_dict[data[i]] * 31 ** i
 print(answer)
