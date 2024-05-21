0577c4033c04cf84b8a45b9cde75aca131fb9023
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2024-05-21 23:58:51 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13956 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String a = "\\    /\\";
-        String b = " )  ( ')";
-        String c = "(  /  )";
-        String d = " \\(__)|";
-
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
+        String s = String.join("\n",
+      "\\    /\\",
+                " )  ( ')",
+                "(  /  )",
+                " \\(__)|");
+        System.out.println(s);
     }
 }
 
