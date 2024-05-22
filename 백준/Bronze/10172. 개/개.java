194cbca757bc12811dbc3b9bf48dd1c8ca785d99
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2024-05-22 22:16:10 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = String.join("\n",
+                "|\\_/|",
+                "|q p|   /}",
+                "( 0 )\"\"\"\\",
+                "|\"^\"`    |",
+                "||_/=\\\\__|");
+        System.out.println(s);
+    }
+}
+
