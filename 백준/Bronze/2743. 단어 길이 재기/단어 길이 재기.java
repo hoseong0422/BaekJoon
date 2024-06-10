63d6512c541a5cc2bdd1a6c55a3ea172fac3d072
Hoseong Lee <70806913+hoseong0422@users.noreply.github.com>
2024-06-10 22:49:19 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String x;
-        x = sc.nextLine();
+        String x = sc.nextLine();
         System.out.println(x.length());
     }
 }
