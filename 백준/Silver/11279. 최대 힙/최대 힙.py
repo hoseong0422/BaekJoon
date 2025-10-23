76a79d0f45197e3a9eb839abcdb73dcac64b8586
Hoseong <hoseong0422@gmail.com>
2025-10-24 00:10:56 +0900

    modify max heap

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,23 +1,21 @@
 import sys
 import heapq
 
-def solve():
-    input = sys.stdin.readline
-    n = int(input().strip())
-    heap = []  # 여기에 -x로 저장 (min-heap을 max-heap처럼 사용)
-    out = []
+input = sys.stdin.readline
+N = int(input().strip())
 
-    for _ in range(n):
-        x = int(input().strip())
-        if x == 0:
-            if heap:
-                out.append(str(-heapq.heappop(heap)))  # 부호 되돌려서 출력
-            else:
-                out.append('0')
+max_heap = [] # 여기에 -x로 저장 (min-heap을 max-heap처럼 사용)
+result = []
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if max_heap:
+            result.append(str(-heapq.heappop(max_heap))) # 부호 되돌려서 출력
         else:
-            heapq.heappush(heap, -x)  # 음수로 넣기
+            result.append('0')
+    else:
+        heapq.heappush(max_heap, -x) # 음수로 넣기
+    
+print('\n'.join(result))
 
-    print('\n'.join(out))
-
-if __name__ == "__main__":
-    solve()
